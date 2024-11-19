98e95df6781fb8b5b75aa7a94231fb0b64b5dc80
Prabhu <vprabhu1771@gmail.com>
2024-11-19 20:56:18 +0530

    Create ProductSeeder.php
    php artisan make:seed ProductSeeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $products= [
+            [
+                'name' => 'Comfort Mattress',
+                'description' => 'A soft and durable mattress.',
+            ]
+        ];
+
+        foreach ($products as $row)
+        {
+            Product::create($row);
+        }
+    }
+}
