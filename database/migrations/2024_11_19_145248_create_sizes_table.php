bef9baaff46c6a8479cb57fcdabd725e4e352208
Prabhu <vprabhu1771@gmail.com>
2024-11-19 20:23:41 +0530

    size model and migration
    php artisan make:model Size -m

---
+++ database/migrations/2024_11_19_145248_create_sizes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sizes', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name'); // Example: "Single", "Queen", "King"
+            $table->string('dimensions')->nullable(); // Optional: "6x3 ft"
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sizes');
+    }
+};
