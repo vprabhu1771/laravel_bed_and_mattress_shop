08e0e3a559f2a53dac35866e05a434333a333590
Prabhu <vprabhu1771@gmail.com>
2024-11-25 21:05:55 +0530

    Thickness Resource added
    php artisan make:filament-resource Thickness --generate

---
+++ app/Filament/Resources/ThicknessResource/Pages/CreateThickness.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ThicknessResource\Pages;
+
+use App\Filament\Resources\ThicknessResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateThickness extends CreateRecord
+{
+    protected static string $resource = ThicknessResource::class;
+}
