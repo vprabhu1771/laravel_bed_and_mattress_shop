614d6390d4554541f532fb50e792175fe9f15049
Prabhu <vprabhu1771@gmail.com>
2024-11-27 13:12:24 +0530

    Mattress List page logic added

--- app/Http/Controllers/MattressController.php
+++ app/Http/Controllers/MattressController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+
 class MattressController extends Controller
 {
     // Mattress List
     public function index(Request $request)
     {
-        return view('frontend/mattress/list/type1');
+
+        $data = [
+            'product' => Product::all()
+        ];
+
+        return view('frontend/mattress/list/type1', $data);
     }
 
 }
--- resources/views/frontend/mattress/list/type1.blade.php
+++ resources/views/frontend/mattress/list/type1.blade.php
@@ -1,4 +1,4 @@
-@extends('frontend.layout.app')
+@extends('frontend/layout/app')
 
 @section('title')
 
@@ -6,8 +6,44 @@ Mattress List
 
 @endsection
 
+
 @section('content')
 
-mattress list
+
+<div class="container mt-4">
+
+    <!-- Search and Filter Form -->
+    <form method="GET" action="{{ route('mattresses.index') }}" class="mb-4">
+        <div class="row mb-3">
+
+            <div class="col-md-4">
+                <input type="text" name="search" class="form-control" placeholder="Search products..." value="{{ request()->input('search') }}">
+            </div>
+            
+            <div class="col-md-4">
+                <button type="submit" class="btn btn-primary">Search</button>
+            </div>
+
+        </div>
+    </form>
+
+    <div class="row">
+        @foreach ($product as $item)
+            <div class="col-md-4 mb-4">
+                <a href="/products/{{ $item->id }}">
+                    <div class="card">
+                        <img src="#" class="card-img-top" alt="{{ $item->name }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $item->name }}</h5>
+                            <p class="card-text">{{ $item->description }}</p>
+                            <!-- <p class="card-text"><strong>${{ $item->price }}</strong></p> -->
+                            <!-- <a href="/cart" class="btn btn-primary">Add to Cart</a> -->
+                        </div>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
