6a75f52895f4d20b4b114dd920ccd2af04f9d5c4
Prabhu <vprabhu1771@gmail.com>
2024-11-27 13:06:48 +0530

    Header Bootstrap UI Added

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -1,1 +1,32 @@
-<h1>Header</h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Small Shop</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            
+            @guest
+            <li class="nav-item">
+                <a class="nav-link" href="/login">Login / Register</a>
+            </li>
+            @endguest
+            
+            @if (Auth::check())
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ Auth::user()->name }} <!-- Display the user's name -->
+                    </a>
+                    <ul class="dropdown-menu">
+                        
+                        
+                    </ul>
+                </li>
+            @endif
+
+        </ul>
+    </div>
+</nav>
